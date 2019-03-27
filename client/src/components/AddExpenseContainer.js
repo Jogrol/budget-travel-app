43bddc7533ee3c7a508c9a6f526c0f3b63dc7114
Joey Grolleman <joeygrolleman@gmail.com>
2019-03-27 17:03:16 +0100

    Created containers

---
+++ client/src/components/AddExpenseContainer.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react'
+import AddExpenseForm from '.AddExpenseForm.js'
+// import PropTypes from 'prop-types'
+
+export class AddExpenseContainer extends Component {
+//   static propTypes = {
+
+//   }
+
+  render() {
+    return (
+      <div>
+          <AddExpenseForm />
+      </div>
+    )
+  }
+}
+
+export default AddExpenseContainer
